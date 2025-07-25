f1ba0e203294f063962916a7cb02eccd46a3df0a
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-07-24 22:39:36 -0700

    navigation bar

---
+++ nav_bar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbox from './nav_box'
+
+export default function Navigation(props) {
+    const { setPageIndex, pageIndex } = props
+
+    // const navButtons = [home, map, calendar, message]
+    const classButtons = ['BIOL 180', 'CHEM 142', 'ENGL 186']
+
+    return (
+        <div className='flex flex-col rounded-r-lg border-gray w-20 min-h-screen items-start gap-2 py-3'>
+            {/* {navButtons.map((item, index) => {
+                return <Navbox key={index} setPageIndex={setPageIndex} pageIndex={pageIndex} index={index}><img src={item} /></Navbox>
+            })} */}
+            <hr className='min-w-15 my-5 mx-auto bg-gray-200 border-1 dark:bg-gray-700' />
+            {classButtons.map((item, index) => {
+                return <Navbox key={index} setPageIndex={setPageIndex} index={0}><p className='text-center text-[13px] leading-tight white'>{item}</p></Navbox>
+            })}
+        </div>
+    )
+}
---
+++ nav_box.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbox(props) {
+    const { children, setPageIndex, pageIndex, index } = props
+    return (
+        <div className={'transition delay-50 rounded-lg w-12 h-12 mx-auto' + (pageIndex === index ? ' bgOrangeSelect' : ' bgOrange')}>
+            <button onClick={() => setPageIndex(index)} className='p-2 transition delay-50 duration-300 hover:-translate-y-1 '>
+                {children}
+            </button>
+        </div>
+    )
+}
