6433a287824719887017761482422fc8f33f8b9b
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-07-28 20:05:52 -0700

    add index page

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import ReactDOM from "react-dom" 
-import './index.css'
+import './index.html'
 import App from './app.tsx'
 
 
--- main_pages/introduction.tsx
+++ main_pages/introduction.tsx
@@ -10,7 +10,52 @@ export default function Intro() {
                 <Header title='Study.Sesh' subtitle='Bringing Students Together' style_h1='text-center transform -translate-x-20 mx-10' style_h2='transform translate-x-10' />
             </div>
             <div>
-                {/* actual intro page !!! */}
+
+    <link
+      rel="cherryblossoms"
+      sizes="180x180"
+      href="./images/cherry_blossoms.jpg"
+    />
+
+    <div className="navbar">
+        <h1>Gabriela Pletosu</h1>
+        <ul>
+            <li className="nextto"><p>
+                <div ref="./images/cherry_blossoms.jpg" className="nextto"></div>
+                <a href="./education.html" className="nextto">Education</a>
+                </p></li>
+            <li className="nextto"><a href="#csclasssection">Computer Science Courses</a></li>
+            <li className="nextto"><a href="./jobs.html" className="nextto">Job Experience</a></li>
+        </ul>
+    </div>
+
+    <div className="block">
+        <div className="nextto">
+            <div className="nextto">
+                <img className="image" src="./images/graduationphoto.jpeg"></img>
+            </div>
+            <div className="nextto">
+                <p className="description">
+                    My name is Gabriela Pletosu. I graduated as a valedictorian from Inglemoor High School in 2022 and am currently double-majoring in Computer Science and Linguistics at UW Seattle.
+                </p>
+            </div>
+        </div>
+    
+        <p>Connect with me at my LinkedIn here! <a href="https://www.linkedin.com/in/GabrielaPletosu/">https://www.linkedin.com/in/GabrielaPletosu/</a></p>
+    
+        <div id="block">
+        </div>
+        <div className="all">
+            <button type="button" onClick='onButtonClick()'>Click to view resume as image</button>
+            <script src="../bkgr.js"></script>
+            <p><a href="./Pletosu_Resume.pdf" download>Or click here to download as PDF!</a></p>
+            
+        
+        
+        <!--extracurricular = harvard + imgs one on edx; that one uni ?? gamer thing??-->
+        </div>
+    </div>
+
             </div>
         </div>
     )
